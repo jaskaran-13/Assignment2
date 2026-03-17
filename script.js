b8b2771f8fb420bc11d5e4022d93318046b5a410
Jaskaran Singh <jaskaransinghv84@gmail.com>
2026-03-16 20:32:38 -0400

    step 15 started code for js create an energy bowl

---
+++ script.js
@@ -0,0 +1,18 @@
+// ==========================================
+// STEP 15: Create an  EnergyBowl Class
+// Jaskaran Singh coding for  Object-Oriented Programming
+// ==========================================
+
+class EnergyBowl {
+
+constructor(size, base, fruits, toppings, protein, sweetener, instructions) {
+
+this.size = size;
+this.base = base;
+this.fruits = fruits;
+this.toppings = toppings;
+this.protein = protein;
+this.sweetener = sweetener;
+this.instructions = instructions;
+
+}
